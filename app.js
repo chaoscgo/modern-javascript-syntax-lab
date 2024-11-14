93d3784d9db058658ec517c8f33b2b796af4ea5f
Beverly <chaoscgo@hotmail.com>
2024-11-14 11:43:39 -0600

    Added Exercises 7 and 9

--- app.js
+++ app.js
@@ -6,11 +6,33 @@ const nums = [13, 87, 2, 89, 12, 4, 90, 63];
 
 // Create a new array where each value is multiplied by 2 and log the new array.
 
+// Your code here
+
 const nums2 = nums.map((currentNum) => {
     return currentNum*2;
 });
 
 console.log(nums2);
 
-//Exercise 7
 
+//Exercise 7
+//See exportingFile.js and importingFile.js
+
+
+//Exercise 9
+
+// Convert the following `if...else` statement in to a ternary:
+
+let pizza = 'tasty';
+
+if (pizza === 'tasty') {
+  console.log('yum');
+} else {
+  console.log('yuck');
+}
+
+// Your code here
+
+let taste = pizza === 'tasty' ? 'yum' : 'yuck';
+
+console.log(taste);
